a81a25bf8f8a068ff1bb04c914895abbe24c67c1
Victor Leandro Eleuterio <victorleandro430@gmail.com>
2025-11-08 15:46:47 -0300

    fix text

--- src/views/Auth/Signin.vue
+++ src/views/Auth/Signin.vue
@@ -15,14 +15,14 @@
               </div>
               <div>
 
-                <div class="relative py-3 sm:py-5">
+                <!-- <div class="relative py-3 sm:py-5">
                   <div class="absolute inset-0 flex items-center">
                     <div class="w-full border-t border-gray-200 dark:border-gray-800"></div>
                   </div>
                   <div class="relative flex justify-center text-sm">
                     <span class="p-2 text-gray-400 bg-white dark:bg-gray-900 sm:px-5 sm:py-2">Ou</span>
                   </div>
-                </div>
+                </div> -->
                 <form>
                   <div class="space-y-5">
                     <!-- Email -->
